8d903acf258eacd3627233589a4615365a7b03db
Carlos Fernandes <chfernandes01@hotmail.com>
2022-02-22 20:37:26 -0300

    Chapter 2 finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 export function App() {
 	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -15,7 +16,7 @@ export function App() {
         setIsNewTransactionModalOpen(false);
     }
 	return (
-		<>
+		<TransactionsProvider>
 			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
 			<Dashboard />
@@ -26,6 +27,6 @@ export function App() {
 			/>
 
 			<GlobalStyle />
-		</>
+		</TransactionsProvider>
 	);
 }
